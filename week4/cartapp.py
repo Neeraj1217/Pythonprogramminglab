30c41c9d44ccdb850aedee83a22c4b8f771f6df0
Neeraj1217 <neeraj1102t@gmail.com>
2025-04-11 19:46:09 +0530

    Add files via upload

---
+++ week4/cartapp.py
@@ -0,0 +1,47 @@
+itemlist=[]
+pricelist=[]
+def additem(item,price):
+  # item=input("Enter the item you want to add to your cart")
+  # price=int(input("Enter the price of the item added"))
+  pricelist.append(price)
+  itemlist.append(item)
+  print(item,"has been successfully added to your cart.")
+
+def removeitem(itemr):
+  if itemr in itemlist:
+    index=itemlist.index(itemr)
+    itemlist.pop(index)
+    pricelist.pop(index)
+    print(itemr,"has been successfully removed from your cart.")
+  else:
+    print("The item you are trying to remove was not found in your cart.")
+
+def viewcart():
+  print("Items in your cart:")
+  for i in itemlist:
+    print(i, end =" ")
+  print()
+  print("Prices of the items:")
+  for j in pricelist:
+    print(j, end = " ")
+  print()
+
+while True:
+  print("Please choose an option:")
+  print("1 - Add an item to your cart")
+  print("2 - Remove an item from your cart")
+  print("3 - View items in your cart")
+  print("4 - Checkout and exit")
+  a=int(input("Enter your choice: "))
+  if(a==1):
+    item=input("Enter the name of the item you wish to add: ")
+    price=int(input("Enter the price of the item: "))
+    additem(item,price)
+  elif(a==2):
+    itemr=input("Enter the name of the item you wish to remove: ")
+    removeitem(itemr)
+  elif(a==3):
+    viewcart()
+  elif(a==4):
+    print("Thank you for shopping with us. Exiting the program...")
+    break
